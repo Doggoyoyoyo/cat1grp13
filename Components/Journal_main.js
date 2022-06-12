d5d1a7a81392cba1d10c6a4256fd94e8f9aa4dfd
idunnowth <tohyicheng1989@gmail.com>
2022-06-12 23:33:03 +0800

    Update Journal Page

--- Components/Journal_main.js
+++ Components/Journal_main.js
@@ -1,4 +1,4 @@
-import 'react-native-gesture-handler'
+import "react-native-gesture-handler";
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
@@ -7,31 +7,46 @@ import { RotateInDownLeft, set } from "react-native-reanimated";
 
 export default function Journal_main() {
   const [chosenemoji, setChosenemoji] = useState(4);
+  const [feelingsubmit, setfeelingsubmit] = useState(4);
+  //feelingsubmit: 0 -> happy, 1-> neutral, 2-> sad, 4 -> not chosen yet.
+
+  const submitHandler = () => {
+    setfeelingsubmit(chosenemoji);
+    console.log(feelingsubmit);
+  };
+
+  const emojisize = 44;
+
   return (
-    <View>
+    <View style={styles.main}>
       <View>
-        <Text>How are you feeling today?</Text>
-        <View>
+        <Text style={styles.qntext}>How are you feeling today?</Text>
+        <View style={styles.emoji_container}>
           <Expression
             feeling="emoji-happy"
             id={0}
             chosenemoji={chosenemoji}
+            emojisize={emojisize}
             setChosenemoji={setChosenemoji}
           />
           <Expression
             feeling="emoji-neutral"
             id={1}
             chosenemoji={chosenemoji}
+            emojisize={emojisize}
             setChosenemoji={setChosenemoji}
           />
           <Expression
             feeling="emoji-sad"
             id={2}
             chosenemoji={chosenemoji}
+            emojisize={emojisize}
             setChosenemoji={setChosenemoji}
           />
         </View>
-        <TouchableOpacity>
+      </View>
+      <View style={[styles.submitbtn, styles.shadowProp]}>
+        <TouchableOpacity onPress={submitHandler}>
           <Text>Submit</Text>
         </TouchableOpacity>
       </View>
@@ -46,24 +61,55 @@ function Expression(props) {
   };
 
   return (
-    <TouchableOpacity onPress={changeemoji}>
-      <Entypo
-        name={props.feeling}
-        size={24}
-        color={props.chosenemoji == props.id ? "blue" : "black"}
-      />
-    </TouchableOpacity>
+    <View>
+      <TouchableOpacity onPress={changeemoji} style={styles.emoji}>
+        <Entypo
+          name={props.feeling}
+          size={props.emojisize}
+          color={props.chosenemoji == props.id ? "blue" : "black"}
+        />
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   main: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#D4FAA4",
     alignItems: "center",
     justifyContent: "center",
   },
   emoji_container: {
     flexDirection: "row",
+    justifyContent: "space-around",
+    backgroundColor: "#D9D9D9",
+    width: "80%",
+    borderRadius: 5,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  emoji: {
+    margin: 10,
+  },
+
+  qntext: {
+    fontSize: 20,
+    //fontFamily: 'Helvetica',
+  },
+  submitbtn: {
+    fontSize: 24,
+    backgroundColor: "#5ECEE7",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 150,
+    height: 50,
+    borderRadius: 5,
+  },
+  shadowProp: {
+    shadowColor: "#171717",
+    shadowOffset: { width: 1, height: 3 },
+    shadowOpacity: 0.4,
+    shadowRadius: 2,
   },
 });
